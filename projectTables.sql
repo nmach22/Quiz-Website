118cc80e2c367d9cf9e7de3ea5dd5418325519d6
eghib22 <eghib22@freeuni.edu.ge>
2024-06-06 18:05:18 +0400

    sql file added

--- projectTables.sql
+++ projectTables.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS quiz_table;
 CREATE TABLE users_table (
                              username VARCHAR(50) PRIMARY KEY,
                              password VARCHAR(50),
-                             admin TINYINT(1) DEFAULT '0'
+                             is_admin TINYINT(1) DEFAULT '0',
+                             user TEXT
 );
 CREATE TABLE quiz_table (
                             quiz_id INT PRIMARY KEY,
@@ -12,5 +13,6 @@ CREATE TABLE quiz_table (
                             is_random TINYINT(1) DEFAULT '0',
                             one_page TINYINT(1) DEFAULT '0',
                             immediate_correction TINYINT(1) DEFAULT '0',
-                            practice_mode TINYINT(1) DEFAULT '0'
+                            practice_mode TINYINT(1) DEFAULT '0',
+                            quiz TEXT
 );
