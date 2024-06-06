cb6f1d8193b1aeb7da17035ec2896096645160eb
asurm22 <asurm22@freeuni.edu.ge>
2024-06-06 20:07:26 +0400

    anouncement and achievement tables added

--- projectTables.sql
+++ projectTables.sql
@@ -9,21 +9,36 @@ CREATE TABLE users_table (
 );
 
 CREATE TABLE quiz_table (
-                            quiz_id INT PRIMARY KEY,
+                            quiz_id INT AUTO_INCREMENT PRIMARY KEY,
                             username VARCHAR(50),
                             is_random TINYINT(1) DEFAULT '0',
                             one_page TINYINT(1) DEFAULT '0',
                             immediate_correction TINYINT(1) DEFAULT '0',
                             practice_mode TINYINT(1) DEFAULT '0',
-                            quiz TEXT
+                            quiz TEXT,
+                            creation_date TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE history_table (
-                            history_id INT PRIMARY KEY,
+                            history_id INT AUTO_INCREMENT PRIMARY KEY,
                             quiz_id INT,
                             username VARCHAR(50),
                             score INT,
                             time TIMESTAMP
+);
+
+CREATE TABLE announcements (
+                            announcement_id INT AUTO_INCREMENT PRIMARY KEY,
+                            user_id         INT,
+                            announcement    varchar(1000),
+                            creation_date   TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE achievements (
+                            achievement_id INT AUTO_INCREMENT PRIMARY KEY,
+                            username varchar(50),
+                            achievement_type varchar(50),
+                            dateAchieved TIMESTAMP DEFAULT NOW()
 );
 
 
