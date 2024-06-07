6c53dc61f1cc19b0f1b0e5400bdb08ce898783d5
asurm22 <asurm22@freeuni.edu.ge>
2024-06-07 13:44:01 +0400

    changes to sql tables

--- projectTables.sql
+++ projectTables.sql
@@ -1,14 +1,14 @@
 USE mysql;
 DROP TABLE IF EXISTS users_table;
 DROP TABLE IF EXISTS quiz_table;
-CREATE TABLE users_table (
+CREATE TABLE users (
                              username VARCHAR(50) PRIMARY KEY,
                              password VARCHAR(50),
                              is_admin TINYINT(1) DEFAULT '0',
                              user TEXT
 );
 
-CREATE TABLE quiz_table (
+CREATE TABLE quizes (
                             quiz_id INT AUTO_INCREMENT PRIMARY KEY,
                             username VARCHAR(50),
                             is_random TINYINT(1) DEFAULT '0',
@@ -19,7 +19,7 @@ CREATE TABLE quiz_table (
                             creation_date TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE history_table (
+CREATE TABLE history (
                             history_id INT AUTO_INCREMENT PRIMARY KEY,
                             quiz_id INT,
                             username VARCHAR(50),
@@ -39,6 +39,21 @@ CREATE TABLE achievements (
                             username varchar(50),
                             achievement_type varchar(50),
                             dateAchieved TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE friends (
+                            username varchar(50),
+                            friend varchar(50),
+                            isRequest TINYINT(1),
+                            requestOrAddDate TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE chat (
+                            username varchar(50),
+                            friend varchar(50),
+                            message varchar(1000),
+                            isRequest TINYINT(1),
+                            sentDate TIMESTAMP DEFAULT NOW()
 );
 
 
